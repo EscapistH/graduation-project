b36f7d3498d6e15bafa3d121dd41706c1cd872db
EscapistH <923737723@qq.com>
2020-04-10 22:27:41 +0800

    审核功能完成

--- client/src/components/admin/Admin.vue
+++ client/src/components/admin/Admin.vue
@@ -13,16 +13,15 @@
           </el-table-column>
           <el-table-column
             :filters="[{text:'管理员', value:'管理员'},{text:'审核人员', value:'审核人员'},{text:'普通用户', value:'普通用户'}]"
-            :filter-method="filter_role"
+            :filter-method="filter_method"
             prop="role"
             min-width="100"
             label="角色"
           ></el-table-column>
           <el-table-column sortable prop="create_time" min-width="200" label="创建时间"></el-table-column>
           <el-table-column sortable prop="last_login_time" min-width="200" label="上次登录时间"></el-table-column>
-          <el-table-column min-width="200" label="操作">
+          <el-table-column min-width="150" label="操作">
             <el-button-group>
-              <el-button @click="edit_user(scope.row)" type="primary" size="mini">编辑</el-button>
               <el-button @click="reset_pwd(scope.row)" type="warning" size="mini">重置密码</el-button>
               <el-button @click="del_user(scope.row)" type="danger" size="mini">删除</el-button>
             </el-button-group>
@@ -42,15 +41,30 @@
           </el-table-column>
           <el-table-column prop="publisher" min-width="60" label="发布人"></el-table-column>
           <el-table-column prop="phone" min-width="80" label="联系电话"></el-table-column>
-          <el-table-column prop="is_review" min-width="50" label="审核状态"></el-table-column>
+          <el-table-column
+            :filters="[{text:'已审核', value:'已审核'},{text:'未审核', value:'未审核'}]"
+            :filter-method="filter_method"
+            prop="is_review"
+            min-width="50"
+            label="审核状态"
+          ></el-table-column>
           <el-table-column prop="reviewer" min-width="60" label="审核人"></el-table-column>
-          <el-table-column prop="is_cancel" min-width="50" label="展示状态"></el-table-column>
+          <el-table-column
+            :filters="[{text:'已撤销', value:'已撤销'},{text:'展示中', value:'展示中'}]"
+            :filter-method="filter_method"
+            prop="is_cancel"
+            min-width="50"
+            label="展示状态"
+          ></el-table-column>
           <el-table-column sortable prop="pub_time" min-width="120" label="发布时间"></el-table-column>
           <el-table-column min-width="60" label="操作">
             <template slot-scope="scope">
-              <el-button-group>
-                <el-button @click="pass_review(scope.row)" type="success" size="mini">通过审核</el-button>
-              </el-button-group>
+              <el-button
+                @click="pass_review(scope.row)"
+                type="success"
+                size="mini"
+                :disabled="scope.row.is_review==='已审核'||scope.row.is_cancel==='已撤销'?true:false"
+              >通过审核</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -61,6 +75,8 @@
 
 <script>
 export default {
+  // 接收父组件Home传来的值
+  props: ["now_login_user_name"],
   data() {
     return {
       all_users_list: [],
@@ -118,7 +134,7 @@ export default {
         }
       );
     },
-    filter_role(value, row, column) {
+    filter_method(value, row, column) {
       let property = column["property"];
       return row[property] === value;
     },
@@ -131,9 +147,28 @@ export default {
     reset_pwd() {
       console.log("reset pwd");
     },
-    pass_review() {
-      // console.log("pass review");
-      console.log(this.all_demands_list);
+    pass_review(row) {
+      this.is_loading_demands = true;
+      this.$http.put("/do_review", { d_id: row.id }).then(
+        res => {
+          // console.log(res);
+          if (res.data.code === 200) {
+            this.is_loading_demands = false;
+            this.$message.success("提交成功");
+            row.is_review = "已审核";
+            row.reviewer = this.now_login_user_name;
+          }
+          if (res.data.code === 401) {
+            this.is_loading_demands = false;
+            this.$message.warning("登录已过期，请重新登录");
+            this.$router.push("/login");
+          }
+        },
+        () => {
+          this.is_loading_demands = false;
+          this.$message.error("提交失败，请重试");
+        }
+      );
     }
   }
 };
--- client/src/components/admin/Review.vue
+++ client/src/components/admin/Review.vue
@@ -14,15 +14,30 @@
           </el-table-column>
           <el-table-column prop="publisher" min-width="60" label="发布人"></el-table-column>
           <el-table-column prop="phone" min-width="80" label="联系电话"></el-table-column>
-          <el-table-column prop="is_review" min-width="50" label="审核状态"></el-table-column>
+          <el-table-column
+            :filters="[{text:'已审核', value:'已审核'},{text:'未审核', value:'未审核'}]"
+            :filter-method="filter_method"
+            prop="is_review"
+            min-width="50"
+            label="审核状态"
+          ></el-table-column>
           <el-table-column prop="reviewer" min-width="60" label="审核人"></el-table-column>
-          <el-table-column prop="is_cancel" min-width="50" label="展示状态"></el-table-column>
+          <el-table-column
+            :filters="[{text:'已撤销', value:'已撤销'},{text:'展示中', value:'展示中'}]"
+            :filter-method="filter_method"
+            prop="is_cancel"
+            min-width="50"
+            label="展示状态"
+          ></el-table-column>
           <el-table-column sortable prop="pub_time" min-width="120" label="发布时间"></el-table-column>
           <el-table-column min-width="60" label="操作">
             <template slot-scope="scope">
-              <el-button-group>
-                <el-button @click="pass_review(scope.row)" type="success" size="mini">通过审核</el-button>
-              </el-button-group>
+              <el-button
+                @click="pass_review(scope.row)"
+                type="success"
+                size="mini"
+                :disabled="scope.row.is_review==='已审核'||scope.row.is_cancel==='已撤销'?true:false"
+              >通过审核</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -33,6 +48,8 @@
 
 <script>
 export default {
+  // 接收父组件Home传来的值
+  props: ["now_login_user_name"],
   data() {
     return {
       all_demands_list: [],
@@ -65,9 +82,28 @@ export default {
         }
       );
     },
-    pass_review() {
-      // console.log("pass review");
-      console.log(this.all_demands_list);
+    pass_review(row) {
+      this.is_loading_demands = true;
+      this.$http.put("/do_review", { d_id: row.id }).then(
+        res => {
+          // console.log(res);
+          if (res.data.code === 200) {
+            this.is_loading_demands = false;
+            this.$message.success("提交成功");
+            row.is_review = "已审核";
+            row.reviewer = this.now_login_user_name;
+          }
+          if (res.data.code === 401) {
+            this.is_loading_demands = false;
+            this.$message.warning("登录已过期，请重新登录");
+            this.$router.push("/login");
+          }
+        },
+        () => {
+          this.is_loading_demands = false;
+          this.$message.error("提交失败，请重试");
+        }
+      );
     }
   }
 };
