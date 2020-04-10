f00f5817e032a5247650c7bb26870457bccac8a5
EscapistH <923737723@qq.com>
2020-04-10 17:46:48 +0800

    bug fix，现在能正确分割数组了

--- client/src/components/demand/MyCancel.vue
+++ client/src/components/demand/MyCancel.vue
@@ -4,7 +4,12 @@
     <el-row :gutter="12" v-for="(each_row, index) in Math.ceil(demands.length/4)" :key="index">
       <!-- 列 -->
       <div v-if="demands.length === 0" style="text-align:center;">再怎么找也没有啦</div>
-      <el-col v-else v-for="(demand, id) in demands.slice(each_row,each_row+4)" :key="id" :span="6">
+      <el-col
+        v-else
+        v-for="(demand, id) in demands.slice((each_row-1)*4,each_row*4)"
+        :key="id"
+        :span="6"
+      >
         <!-- 卡片 -->
         <div class="card-box">
           <!-- 鼠标覆盖时出现卡片阴影 -->
