a461369ab34fd2948dc1ba57f9de8789f47072be
EscapistH <923737723@qq.com>
2020-04-10 22:33:14 +0800

    添加获取全部需求的函数，原来获取全部未撤销的需求的函数改为更易识别的名称

--- server/app/views/demands.py
+++ server/app/views/demands.py
@@ -12,12 +12,69 @@ from app.views.public import get_token_and_id
 demands = Blueprint('demands', __name__)
 
 
+@demands.route('/all_demands', methods=['GET'])
+def get_all_demands():
+    if request.method == 'GET':
+        sql = '''
+        select
+            t2.d_id,
+            t2.d_title,
+            t2.d_content,
+            t2.publisher,
+            t2.u_phone,
+            t2.d_pub_time,
+            t2.d_is_review,
+            t1.reviewer,
+            t2.d_is_cancel
+        from
+            (
+            select
+                t2.d_id,
+                t2.d_title,
+                t2.d_content,
+                coalesce(t1.u_name, t1.u_nick) as publisher,
+                t1.u_phone,
+                t2.d_pub_time,
+                t2.d_is_review,
+                t2.d_reviewer,
+                t2.d_is_cancel
+            from
+                (select u_id, u_nick, u_name, u_phone from app.users) as t1
+            right join
+                (select d_id, d_title, d_content, d_publisher, d_pub_time, d_is_review, d_reviewer, d_is_cancel from app.demands) as t2
+            on
+                t1.u_id = t2.d_publisher
+            ) as t2
+        left join
+            (select u_id, coalesce(u_name, u_nick)as reviewer from app.users) as t1
+        on
+            t2.d_reviewer = t1.u_id
+        order by
+            d_pub_time desc
+        '''
+        rs = db.session.execute(sql).fetchall()
+        data = [
+            {
+                'id': r[0],
+                'title': r[1],
+                'content': json.loads(r[2]),
+                'publisher': r[3],
+                'phone': r[4],
+                'pub_time': str(r[5]),
+                'is_review': '已审核' if r[6] else '未审核',
+                'reviewer': r[7],
+                'is_cancel': '已撤销' if r[8] else '展示中'
+            } for r in rs
+        ]
+        return ResponseResult.get_result('Success', data)
+
+
 @demands.route('/demands', methods=['GET'])
-def get_some_demands():
+def get_some_no_cancel_demands():
     if request.method == 'GET':
         # 获取请求参数
-        num = int(request.args['num'])
-        page = int(request.args['num']) * int(request.args['page'])
+        num = int(request.args['num']) if request.args['num'] else 1000
+        page = int(request.args['num']) * int(request.args['page']) if request.args['page'] else 0
         # print(num, page)
         sql = '''
         select
@@ -50,7 +107,7 @@ def get_some_demands():
                 t1.u_id = t2.d_publisher
             ) as t2
         left join
-            (select u_id,case when u_name is null then u_nick end as reviewer from app.users) as t1
+            (select u_id, coalesce(u_name, u_nick)as reviewer from app.users) as t1
         on
             t2.d_reviewer = t1.u_id
         order by
@@ -181,7 +238,7 @@ def do_publish():
         if not TokenOperate.check_token(token, u_id):
             return ResponseResult.get_result('Declined')
         if len((request.json['title']).strip()) == 0:
-            return ResponseResult.get_result('Error', [{'msg':''}])
+            return ResponseResult.get_result('Error', [{'msg': ''}])
         d_title = request.json['title']
         d_publisher = int(request.json['publisher'])
         d_content = json.dumps(request.json['content'], ensure_ascii=False)
@@ -190,7 +247,8 @@ def do_publish():
         sql = '''
                 insert into app.demands(d_title, d_content, d_publisher, d_pub_time) values(:d_title, :d_content, :d_publisher, :d_pub_time)
                 '''
-        db.session.execute(sql, {'d_title': d_title, 'd_content': d_content, 'd_publisher': d_publisher, 'd_pub_time': d_pub_time})
+        db.session.execute(sql, {'d_title': d_title, 'd_content': d_content, 'd_publisher': d_publisher,
+                                 'd_pub_time': d_pub_time})
         return ResponseResult.get_result('Success')
 
 
@@ -220,4 +278,17 @@ def cancel_demand_by_id():
         update app.demands set d_is_cancel = true where d_id = :d_id
         '''
         db.session.execute(sql, {'d_id': d_id})
+        return ResponseResult.get_result('Success')
+
+
+@demands.route('/do_review', methods=['PUT'])
+def do_review():
+    if request.method == 'PUT':
+        token, u_id = get_token_and_id()
+        if not TokenOperate.check_token(token, u_id):
+            return ResponseResult.get_result('Declined')
+        d_id = int(request.json['d_id'])
+        reviewer = int(request.headers['uid'])
+        sql = 'update app.demands set d_is_review = true, d_reviewer = :reviewer where d_id = :d_id'
+        db.session.execute(sql, {'reviewer': reviewer, 'd_id': d_id})
         return ResponseResult.get_result('Success')
